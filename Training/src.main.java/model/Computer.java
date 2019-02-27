152534fbcb291a2b73c90b51b2681f26e0408b37
JakubSkorupinski96 <jskorupinski@excilys.com>
2019-02-28 01:04:25 +0800

    [WIP] added pattern builder

--- Training/src.main.java/model/Computer.java
+++ Training/src.main.java/model/Computer.java
@@ -11,7 +11,6 @@ public class Computer {
   private Company company;
   private String companyName;
 
-
   /**
    * . Constructeur vide de computer
    */
@@ -21,14 +20,15 @@ public class Computer {
   /**
    * . Constructeur de computer
    *
-   * @param id : id de l'ordinateur
-   * @param name : nom de l'ordinateur
-   * @param introDate : date d'introduction de l'ordinateur
+   * @param id               : id de l'ordinateur
+   * @param name             : nom de l'ordinateur
+   * @param introDate        : date d'introduction de l'ordinateur
    * @param discontinuedDate : date d'arrêt de production de l'ordinateur
-   * @param company : companie associé à l'id_company de l'ordinateur
-   * @param companyName : nom de cette companie
+   * @param company          : companie associé à l'id_company de l'ordinateur
+   * @param companyName      : nom de cette companie
    */
-  public Computer(int id, String name, Date introDate, Date discontinuedDate, Company company, String companyName) {
+  public Computer(int id, String name, Date introDate, Date discontinuedDate, Company company,
+      String companyName) {
     this.id = id;
     this.name = name;
     this.introduced = introDate;
@@ -89,5 +89,101 @@ public class Computer {
   public String toString() {
     return "Computer [id=" + id + ", name=" + name + ", intro_date=" + introduced
         + ", discontinued_date=" + discontinued + ", company=" + companyName + " ]";
+  }
+
+  public static class Builder {
+
+    private int id;
+    private String name;
+    private Date introduced;
+    private Date discontinued;
+    private Company company;
+    private String companyName;
+
+    /**
+     * . Builder's constructor
+     *
+     * @param id : computer's id
+     */
+
+    public Builder(int id) {
+      this.id = id;
+    }
+
+    /**
+     * . Adds a computer name to the builder
+     *
+     * @param name : computer's name
+     * @return Builder
+     */
+
+    public Builder computerName(String name) {
+      this.name = name;
+      return this;
+    }
+
+    /**
+     * . Adds an introduction date to the builder
+     *
+     * @param introduced : a computer's introduction date
+     * @return Builder
+     */
+
+    public Builder computerIntroduced(Date introduced) {
+      this.introduced = introduced;
+      return this;
+    }
+
+    /**
+     * . Adds a discontinuation date to the builder
+     *
+     * @param discontinued : a computer's discontinuation date
+     * @return Builder
+     */
+
+    public Builder computerDiscontinued(Date discontinued) {
+      this.discontinued = discontinued;
+      return this;
+    }
+
+    /**
+     * . Adds a computer's company to the builder
+     *
+     * @param company : a computer's company
+     * @return Builder
+     */
+
+    public Builder computerCompany(Company company) {
+      this.company = company;
+      return this;
+    }
+
+    /**
+     * . Adds a computer's company's name to the builder
+     *
+     * @param companyName : the company name
+     * @return Builder
+     */
+
+    public Builder computerCompanyName(String companyName) {
+      this.companyName = companyName;
+      return this;
+    }
+
+    /**
+     * . Builds a computer with the builder's values
+     * @return ComputerS
+     */
+
+    public Computer build() {
+      Computer computer = new Computer();
+      computer.id = this.id;
+      computer.name = this.name;
+      computer.introduced = this.introduced;
+      computer.discontinued = this.discontinued;
+      computer.company = this.company;
+      computer.companyName = this.companyName;
+      return computer;
+    }
   }
 }

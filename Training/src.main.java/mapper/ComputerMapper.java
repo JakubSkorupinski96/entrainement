0e5856f44d13b12c5614aacce9557fd8565970bf
JakubSkorupinski96 <jskorupinski@excilys.com>
2019-03-08 20:49:03 +0100

    [FIX] better computer mapper methods

--- Training/src.main.java/mapper/ComputerMapper.java
+++ Training/src.main.java/mapper/ComputerMapper.java
@@ -1,75 +1,72 @@
 package mapper;
 
-import java.sql.Date;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.List;
 
 import model.Company;
 import model.Computer;
+
+import dto.ComputerDTO;
 
 public class ComputerMapper {
 
   Computer computer;
   Company company;
 
+  private static ComputerMapper instance;
+  
+  public ComputerMapper getInstance() {
+    if (instance == null) {
+      instance =  new ComputerMapper();
+    }
+    return instance;
+  }
+  
   /**
    * . Contructeur de ComputerMapper
    */
-  public ComputerMapper() {
-  }
-
-  /**
-   * . Crée une company
-   *
-   * @param id : id de la compagnie
-   * @param companyName : nom de la compagnie
-   *
-   * @return Company
-   */
-
-  public Company createCompany(int id, String companyName) {
-    return company = new Company(id, companyName);
+  private ComputerMapper() {
   }
   
-  public List<String> companyToDTO(Company company) {
-    List<String> listCompany = new ArrayList<>();
-    String id = Integer.toString(company.getId());
-    String name = company.getName();
-    listCompany.add(id);
-    listCompany.add(name);
-    return listCompany;
-  }
-
-  /**
-   * . Crée un computer
-   *
-   * @param id : id du computer
-   * @param name : nom du computer
-   * @param introduced : date de sortie du computer
-   * @param discontinued : date d'arrêt de production du computer
-   * @param company : compagnie associé au computer
-   * @param companyName : nom de la compagnie
-   *
-   * @return Computer
-   */
-  public Computer createComputer(int id, String name, LocalDate introduced, LocalDate discontinued, Company company, String companyName) {
-    return computer = new Computer(id, name, introduced, discontinued, company, companyName);
+  public ComputerDTO computerToDTO(Computer computer){
+    ComputerDTO computerDTO = new ComputerDTO();
+    computerDTO.setId(computer.getId());
+    computerDTO.setName(computer.getName());
+    computerDTO.setIntroduced(computer.getIntroduced().toString());
+    computerDTO.setDiscontinued(computer.getDiscontinued().toString());
+    computerDTO.setCompanyName(computer.getCompanyName());
+    return computerDTO;
   }
   
-  public List<String> computerToDTO(Computer computer){
-    List<String> listComputer = new ArrayList<>();
-    String id = Integer.toString(computer.getId());
-    String name = computer.getName();
-    String introduced = computer.getIntroduced().toString();
-    String discontinued = computer.getDiscontinued().toString();
-    String companyName = computer.getCompanyName();
-    listComputer.add(id);
-    listComputer.add(name);
-    listComputer.add(introduced);
-    listComputer.add(discontinued);
-    listComputer.add(companyName);
-    return listComputer;
+  public Computer DTOToComputer(ComputerDTO computerDTO) {
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:SS");
+    Computer computer = new Computer();
+    computer.setId(computerDTO.getId());
+    computer.setName(computerDTO.getName());
+    computer.setIntroduced(LocalDate.parse(computerDTO.getIntroduced(),formatter));
+    computer.setDiscontinued(LocalDate.parse(computerDTO.getDiscontinued(),formatter));
+    computer.setCompanyName(computerDTO.getCompanyName());
+    return computer;
+  }
+  
+  public ArrayList<ComputerDTO> computersToDTOs (ArrayList<Computer> computers){
+    ArrayList<ComputerDTO> computerDTOs = new ArrayList<>();
+    for (Computer computer : computers) {
+      ComputerDTO computerDTO = computerToDTO(computer);
+      computerDTOs.add(computerDTO);
+    }
+    return computerDTOs;
+  }
+  
+  public ArrayList<Computer> DTOsToComputers (ArrayList<ComputerDTO> computerDTOs){
+    ArrayList<Computer> computers = new ArrayList<>();
+    for (ComputerDTO computerDTO : computerDTOs) {
+      
+      Computer computer = DTOToComputer(computerDTO);
+      computers.add(computer);
+    }
+    return computers;
   }
 
 }
